b6ec0007ef002d723ae735f4d01a4aab26c1291f
AmnestyCloud <76400077+AmnestyCloud@users.noreply.github.com>
2021-08-02 15:07:50 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,2 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'hi this is a README file',
-    owner   => 'root',
-  }
-  file {'/root/README:
-    owner => 'root',
-  }
 }
